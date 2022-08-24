be1bfcd09343569103c819b678852d862caff5d1
Oleksandr Korniienko <korniienko.alexander@gmail.com>
2022-08-24 16:54:08 +0300

    Unit of work base. Removed wrong presentation layer set up

---
+++ DataAccessLayer/InformMedia.Repository.Implementation/Repositories/BaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using InformMedia.Repository.Contracts;
+
+namespace InformMedia.Repository.Implementation.Repositories
+{
+    public class BaseRepository : IBaseRepository<InformMediaContext>
+    {
+        public void SetContext(InformMediaContext context)
+        {
+            this.Context = context;
+        }
+
+        protected InformMediaContext Context { get; private set; }
+    }
+}
---
+++ DataAccessLayer/InformMedia.Repository.Implementation/UnitOfWork/UnitOfWorkFactory.cs
@@ -0,0 +1,24 @@
+﻿using Autofac.Features.Metadata;
+using InformMedia.Repository.Contracts.Factories;
+using InformMedia.Repository.Contracts.UnitOfWork;
+using InformMedia.Repository.Contracts;
+
+namespace InformMedia.Repository.Implementation.UnitOfWork
+{
+    public class UnitOfWorkFactory : IUnitOfWorkFactory
+    {
+        private readonly Func<Meta<Lazy<IBaseRepository<InformMediaContext>>>>[] repositoryFactories;
+        private readonly IGenericFactory factory;
+
+        public UnitOfWorkFactory(IGenericFactory factory, Func<Meta<Lazy<IBaseRepository<InformMediaContext>>>>[] repositoryFactories)
+        {
+            this.factory = factory;
+            this.repositoryFactories = repositoryFactories;
+        }
+
+        public IUnitOfWork Create()
+        {
+            return new UnitOfWork(this.factory, this.repositoryFactories);
+        }
+    }
+}
